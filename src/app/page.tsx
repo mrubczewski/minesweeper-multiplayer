9d0f46637211451581c937051f4cc6b8acb8f426
Marek Rubczewski <m.rubczewski@gmail.com>
2024-07-23 20:06:16 +0200

    Add initial component files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <p>Minesweeper</p>
+            <div className={styles.actionContainer}>
+                <button className={styles.actionButton}>Find Opponent</button>
+                <button className={styles.actionButton}>Create a game</button>
+            </div>
         </main>
     )
 }
